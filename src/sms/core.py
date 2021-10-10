9f50b2260d84092b41557002516df6853f1cc04c
hentai-chan <70948244+hentai-chan@users.noreply.github.com>
2021-10-10 22:33:40 +0200

    Fix debug test workflow

--- src/sms/core.py
+++ src/sms/core.py
@@ -9,11 +9,10 @@ from .config import BRIGHT, GREEN, RESET_ALL, YELLOW
 
 
 def send_sms(sid: str, token: str, from_: str, to: str, msg: str, debug: bool) -> None:
-    client = Client(sid, token)
-
     if debug:
         utils.print_on_warning(f"Debug mode enabled. Sent message {BRIGHT}{GREEN}{msg}{RESET_ALL} to {to}")
         return
 
+    client = Client(sid, token)
     message = client.messages.create(body=msg, from_=from_, to=to)
     utils.print_on_success(f"Message sent on {BRIGHT}{YELLOW}{dt.today().strftime('%c')}{RESET_ALL}")
